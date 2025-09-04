43b22d33f32e339e4057f8a6d1c736c3c1667a9e
partcanadian <venkat.muthaly@outlook.com>
2025-09-04 15:10:51 -0400

    test

---
+++ students.py
@@ -0,0 +1,21 @@
+class Students:
+    def __init__(self, name, student_id, registered_courses):
+        self.name = name
+        self.student_id = student_id
+        self.registered_courses = registered_courses
+    
+    def register_for_course(self, course_code):
+        if course_code not in self.registered_courses:
+            self.registered_courses.append(course_code)
+            return self.registered_courses
+        return f"Student {self.name} is already registered for {course_code}"
+    
+    def drop_course(self, course_code):
+        if course_code in self.registered_courses:
+            self.registered_courses.remove(course_code)
+            return f"Student {self.name} dropped {course_code}"
+        return f"Student {self.name} is not registered for {course_code}"
+    
+    def get_registered_courses(self):
+        return self.registered_courses
+    
